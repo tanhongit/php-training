a43596553d6ce664e78b17c07b9f4010da4bf9cd
tanhongit <phuongtan12357nguyen@gmail.com>
2020-10-21 03:01:27 +0700

    add features

---
+++ delete.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+include('functions.php');
+
+$user_id = intval($_GET['user_id']);
+if($_SESSION['user']['user_type'] == "admin"){
+    if($_SESSION['user']['id'] != $user_id){
+        user_delete($user_id);
+    }
+}
+header('location:list.php?list=dgsre');
---
+++ edit.php
@@ -0,0 +1,59 @@
+<?php 
+session_start();
+
+$user_id = intval($_GET['edit']);
+
+include('functions.php');
+
+$result = [];
+$userName = "";
+$fullName = "";
+$userEmail = "";
+if (isset($_GET['edit'])) {
+    if(isLoggedIn()){
+        $query = "SELECT * FROM users WHERE id=" . $user_id;
+        $result = mysqli_query($conn, $query);
+        $data = mysqli_fetch_assoc($result);
+    }
+}
+?>
+
+<html>
+<head>
+    <title>Register</title>
+    <link rel="stylesheet" href="public/css/styles.css">
+</head>
+<body>
+<div class="header">
+	<h2>Edit User</h2>
+</div>
+</form>
+
+<form method="post" action="">
+	<?php echo display_error(); ?>	
+        
+        <div class="input-group">
+            <label>Username</label>
+            <input type="text" name="username1" value="<?php echo $data['username']; ?>" placeholder="<?php echo $data['username']; ?>">
+        </div>
+        <div class="input-group">
+            <label>Full Name</label>
+            <input type="text" name="fullname1" value="<?php echo $data['fullname']; ?>" placeholder="<?php echo $data['fullname']; ?>">
+        </div>
+        <div class="input-group">
+            <label>Email</label>
+            <input type="email" name="email1" value="<?php echo $data['email']; ?>" placeholder="<?php echo $data['email']; ?>">
+        </div>
+        <div class="input-group">
+            <button type="submit" class="btn" name="save_btn">Save</button>
+        </div>
+
+</form>
+<div class="back" style="text-align: center">
+    <input type="button" value="Back" onClick="javascript:history.go(-2)" />
+</div>
+	
+
+
+</body>
+</html>
