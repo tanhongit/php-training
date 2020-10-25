2657492ff4aabe71a2167ed3ed1b8f0ccb516f83
Thuận Trần <thuankg1752@gmail.com>
2020-10-26 01:46:36 +0700

    confirm user

--- register.php
+++ register.php
@@ -15,7 +15,7 @@ include('functions.php')
 	<div class="header">
 		<h2>Register</h2>
 	</div>
-	<form method="post" action="register.php">
+	<form method="post" action="confirm-user/request.php">
 		<?php echo display_error(); ?>
 		<div class="input-group">
 			<label>Username</label>
