056a340ac41a396f474556a15698a582ee6b1ab9
tanhongit <phuongtan12357nguyen@gmail.com>
2020-11-22 23:59:42 +0700

    upload mssv to database

--- for-extension/request.php
+++ for-extension/request.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
-include('finctions.php');
+include('../functions.php');
 if (isset($_GET['mssv'])) {
     $presence = array(
+        'id' => 0,
         'presence_mssv' => escape($_GET['mssv']),
         'presence_time' => gmdate('Y-m-d H:i:s', time() + 7 * 3600)
     );
