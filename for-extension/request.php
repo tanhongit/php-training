017cc8537dfba7ac2232a64e29d83e24cb0a356c
tanhongit <phuongtan12357nguyen@gmail.com>
2020-11-23 02:26:46 +0700

    upload preseence status to database

--- for-extension/request.php
+++ for-extension/request.php
@@ -2,10 +2,21 @@
 session_start();
 include('../functions.php');
 if (isset($_GET['mssv'])) {
-    $presence = array(
-        'id' => 0,
-        'presence_mssv' => escape($_GET['mssv']),
-        'presence_time' => gmdate('Y-m-d H:i:s', time() + 7 * 3600)
+    $users_option = array(
+        'order_by' => 'id DESC'
     );
-    save('presence', $presence);
+    $users = get_by_options('users', $users_option);
+    foreach ($users as $user) {
+        if ($user['id'] == $_GET['mssv']) {
+            $id = $_GET['mssv'];
+        }
+    }
+    if (isset($id)) {
+        $presence = array(
+            'id' => $id,
+            'presence' => 1,
+            'presence_time' => gmdate('Y-m-d H:i:s', time() + 7 * 3600)
+        );
+        save('users', $presence);
+    }
 }
